1fc3960f23051586b73d7101601f59d4e53bb057
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-07-20 21:17:50 -0400

    agregado views

---
+++ src/views/DescripcionView.vue
@@ -0,0 +1,89 @@
+<script setup>
+</script>
+
+<template>
+    <!-- Header-->
+    <header class="bg-primary bg-gradient text-white pb-5 pt-5">
+        <div class="container px-4 text-center">
+            <h1 class="fw-bolder">Información general</h1>
+            <p class="lead">
+                Esta sección lo ayudará a saber más sobre el proyecto y su modo de uso. Presione "comenzar"
+                para iniciar sesión.
+            </p>
+            <a class="btn btn-lg btn-light" href="">Comenzar</a>
+        </div>
+    </header>
+    <!-- About section-->
+    <section id="about" class="pt-5 pb-5">
+        <div class="container px-4">
+            <div class="row">
+                <div class="col-lg">
+                    <div class="pt-3 pb-3">
+                        <h4> ¿De qué trata este proyecto? </h4>
+                    </div>
+                    <p class="text-justify">
+                        Trata sobre el proceso de extracción de características de archivos de audios. 
+                        Pensado en aquellas personas que necesitan entrenar redes neuronales convolucionales (CNNs)
+                        clasificadoras de audios, basadas por ejemplo en imagénes de espectrogramas o directamente
+                        usando las matrices de distribución de energía,pero no tienen el conocimiento o herramientas
+                        para generarlos.
+                    </p>
+                </div>
+                <div class="col-lg">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg">
+                </div>
+                <div class="col-lg">
+                    <h4 class="pt-3 pb-3"> ¿En qué consiste? </h4>
+                    <p class="text-justify"> 
+                        Consiste en una aplicación web, donde los usuarios deberán registrarse en ella para acceder a las
+                        funcionalidades. Este, podrá elegir algunas características a extraer, por ejemplo: espectrogramas,
+                        espectrogramas de MEL, amplitud o coeficientes cepstrales de MEL (MFCCs). Luego de ello, deberá
+                        cargar un archivo de audio en la aplicación en formato .wav o .mp3, ajustará algunos parámetros
+                        usados por los extractores de características que generarán una imagen con el contenido deseado,
+                        para finalmente descargar el resultado como una imagen o un archivo generado por el módulo pickle
+                        de python.
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg">
+                    <h4 class="pt-3 pb-3"> Navegación</h4>
+                    <p class="text-justify"> 
+                        La barra de navegación de la aplicación, contiene tres opciones:
+                    </p>
+                    <ol>
+                        <li> Inicio: Redirige a la vista de inicio de la página. </li>
+                        <li> Descripción: Corresponde a esta sección. </li>
+                        <li> 
+                            Características: Esta sección muestra los tipos de características que
+                            pueden ser extraidos de un archivo de audio.
+                        </li>
+                    </ol>
+                </div>
+                <div class="col-lg">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg"></div>
+                <div class="col-lg">
+                    <h4 class="pt-3 pb-3"> ¿Cómo extraer características? </h4>
+                    <ol>
+                        <li>Inicie sesión, en caso de no estar registrado, primero registrese.</li>
+                        <li>En la barra de navegación pulse Características.</li>
+                        <li>
+                            A continuación, podrá ver varias opciones que indican el tipo de característica a extraer,
+                            apriete el botón ¡Comenzar! para iniciar el proceso.
+                        </li>
+                        <li>Cargue su archivo de audio, en formato .wav o .mp3.</li>
+                        <li>Ajuste los parámetros solicitados.</li>
+                        <li>Descargue los resultados en los formatos establecidos.</li>
+                        <li>De ser necesario guarde el archivo de audio y/o los resultados en la aplicación.</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,140 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+  import { RouterLink } from 'vue-router'
+  import SoundCharacteristics from '../components/cards/SoundCharacteristics.vue';
+  import GenericFeature from '../components/features/GenericFeature.vue';
+
+  const toInformacion = { name: 'informacion' }
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <!-- Header-->
+  <header class="bg-dark py-5">
+      <div class="container px-5">
+          <div class="row gx-5 align-items-center justify-content-center">
+              <div class="col-lg-8 col-xl-7 col-xxl-6">
+                  <div class="my-5 text-center text-xl-start">
+                      <h1 class="display-5 fw-bolder text-white mb-2">Extractor de características de audios online</h1>
+                      <p class="lead fw-normal text-white-50 mb-4">Extraiga de manera sencilla información relevante de sus archivos de audios!</p>
+                      <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
+                          <a class="btn btn-primary btn-lg px-4 me-sm-3" href="">Comenzar</a>
+                          <RouterLink class="btn btn-outline-light btn-lg px-4" :to="toInformacion">Ver más</RouterLink>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center">
+                  <div id="carouselExampleDark" class="carousel carousel-dark slide pb-5 pl-5 pr-5 pt-3 mb-5 ml-5 mr-5 mt-3" data-bs-ride="carousel">
+                      <div class="carousel-indicators">
+                          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                      </div>
+                      <div class="carousel-inner">
+                          <div class="carousel-item active" data-bs-interval="4000">
+                          <img src="" class="d-block m-auto" width="640" height="320">
+                          <div class="carousel-caption d-none d-md-block">
+                              <h5>Extraiga características de audio</h5>
+                              <p>Obtenga extracción de características.</p>
+                          </div>
+                          </div>
+                          <div class="carousel-item" data-bs-interval="4000">
+                          <img src="" class="d-block m-auto" width="640" height="320">
+                          <div class="carousel-caption d-none d-md-block">
+                              <h5>Obtenga varias características</h5>
+                              <p>Espectrograma y MFCC.</p>
+                          </div>
+                          </div>
+                          <div class="carousel-item" data-bs-interval="4000">
+                          <img src="" class="d-block m-auto" width="640" height="320">
+                          <div class="carousel-caption d-none d-md-block">
+                              <h5>Todo en python</h5>
+                              <p>Implementado en python.</p>
+                          </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </header>
+  <!-- Features section-->
+  <section class="py-5" id="features">
+      <div class="container px-5 my-5">
+          <div class="row gx-5">
+              <div class="col-lg-4 mb-5 mb-lg-0"><h2 class="fw-bolder mb-0">Algunas ventajas.</h2></div>
+              <div class="col-lg-8">
+                  <div class="row gx-5 row-cols-1 row-cols-md-2">
+                      <div class="col mb-5 h-100">
+                        <GenericFeature iconClass="bi bi-collection" title="Varias características"
+                          description="Puede generar espectrogramas, espectrogramas de MEL,
+                          series de tiempo de amplitud y coeficientes cepstrales en la frecuencia de MEL." />
+                      </div>
+                      <div class="col mb-5 h-100">
+                        <GenericFeature iconClass="bi bi-download" title="Descargar Resultados"
+                            description="Puede descargarlos como imágenes o archivos con objetos python." />
+                      </div>
+                      <div class="col mb-5 mb-md-0 h-100">
+                        <GenericFeature iconClass="bi bi-file-earmark-code" title="Directamente en python"
+                             description="Genere matrices de distribución
+                            de energía que pueden ser cargados directamente en python usando los modúlos pickle y numpy." />
+                      </div>
+                      <div class="col h-100">
+                        <GenericFeature iconClass="bi bi-check-all" title="Sencillo"
+                             description="Ajuste los parámetros necesarios y genere resultados de manera rápida y sencilla." />
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+  <!-- Blog preview section-->
+  <section class="py-5">
+      <div class="container px-5 my-5">
+          <div class="row gx-5 justify-content-center">
+              <div class="col-lg-8 col-xl-6">
+                  <div class="text-center">
+                      <h2 class="fw-bolder">Nuestros resultados</h2>
+                      <p class="lead fw-normal text-muted mb-5">De manera sencilla podrá tener resultados como los siguientes.</p>
+                  </div>
+              </div>
+          </div>
+          <div class="row gx-5">
+              <div class="col-lg-4 mb-5">
+                <SoundCharacteristics cardTipe="Imagen" cardTitle="Espectrograma"
+                  cardDescription="Resultado obtenido a partir de 2 seg de audio." soundSrc="Generado con audio de abejas"
+                  soundFileExt="Extensión: mp3 &middot; Muestreo: 44100" />
+              </div>
+              <div class="col-lg-4 mb-5">
+                <SoundCharacteristics cardTipe="Imagen" cardTitle="Espectrograma de MEL"
+                  cardDescription="Resultado obtenido a partir de 2 seg de audio." soundSrc="Generado con audio de abejas"
+                  soundFileExt="Extensión: mp3 &middot; Muestreo: 44100" />
+              </div>
+              <div class="col-lg-4 mb-5">
+                <SoundCharacteristics cardTipe="Imagen" cardTitle="Coeficientes cepstrales en la frecuencia de MEL"
+                  cardDescription="Resultado obtenido a partir de 2 seg de audio." soundSrc="Generado con audio de abejas"
+                  soundFileExt="Extensión: mp3 &middot; Muestreo: 44100" />
+              </div>
+          </div>
+          <!-- Call to action-->
+          <a name="registrarse"></a>
+          <aside class="bg-primary bg-gradient rounded-3 p-4 p-sm-5 mt-5">
+              <div class="d-flex align-items-center justify-content-between flex-column flex-xl-row text-center text-xl-start">
+                  <div class="mb-4 mb-xl-0">
+                      <div class="fs-3 fw-bold text-white">Registrate.</div>
+                      <div class="text-white-50">Comience a usar las funcionalidades de la aplicación.</div>
+                  </div>
+                  <div class="ms-xl-4">
+                      <form class="text-center text-white" method="POST" action="">
+                          <div class="mb-3">
+                              <input type="email" class="form-control" placeholder="Correo electrónico" id="correo" name="email">
+                          </div>
+                          <div class="mb-3">
+                              <input type="password" class="form-control" placeholder="Contraseña" id="contrasenna" name="password">
+                          </div>
+                          <button class="btn btn-outline-light" id="button-newsletter" type="submit">Registrame</button>
+                      </form>
+                  </div>
+              </div>
+          </aside>
+      </div>
+  </section>
 </template>
