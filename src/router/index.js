eadaa7b960fefea7e17617b117cb7ed08a4d63f4
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-07-20 21:18:21 -0400

    modificaciones para inclusión de views y componentes

--- src/router/index.js
+++ src/router/index.js
@@ -10,14 +10,22 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/contacto',
+      name: 'contacto',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/ContactoView.vue')
+    },
+    {
+      path: '/informacion',
+      name: 'informacion',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/DescripcionView.vue')
     }
-  ]
+  ],
 })
 
 export default router
