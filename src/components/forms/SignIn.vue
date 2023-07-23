47ba8a506476eabe721c2cc9033a34f489d489d0
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-07-23 16:12:11 -0400

    agregado formularios

---
+++ src/components/forms/SignIn.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container mt-5 mb-5 pt-5 pb-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card-group mb-0">
+                    <div class="card p-4">
+                        <div class="card-body">
+                            <form method="POST" action="{% url 'login' %}">
+                                <h1>Iniciar sesión</h1>
+                                <p class="text-muted">Ingresa tus datos e inicia sesión</p>
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text"><i class="bi bi-person-fill"></i></span>
+                                    <input type="email" class="form-control" placeholder="Correo electrónico" name="email">
+                                </div>
+                                <div class="input-group mb-4">
+                                    <span class="input-group-text"><i class="bi bi-lock-fill"></i></span>
+                                    <input type="password" class="form-control" placeholder="Contraseña" name="password">
+                                </div>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <button type="submit" class="btn btn-primary px-4">Iniciar sesión</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">
+                        <div class="card-body text-center">
+                            <div>
+                                <h2>¿Estás registrado?</h2>
+                                <p>Crea una cuenta e inicia sesión para acceder a las funcionalidades de la aplicación.</p>
+                                <a href="{% url 'inicio' %}#registrarse" class="btn btn-primary active mt-3">Registrarme
+                                    ahora</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/forms/SignUp.vue
@@ -0,0 +1,27 @@
+<script setup>
+    const props = defineProps({
+        title: 'string',
+        description: 'string',
+        titleButtonSubmit: 'string'
+    })
+</script>
+
+<template>
+    <div class="d-flex align-items-center justify-content-between flex-column flex-xl-row text-center text-xl-start">
+        <div class="mb-4 mb-xl-0">
+            <div class="fs-3 fw-bold text-white"> {{ title }} </div>
+            <div class="text-white-50"> {{ description }} </div>
+        </div>
+        <div class="ms-xl-4">
+            <form class="text-center text-white" method="POST" action="">
+                <div class="mb-3">
+                    <input type="email" class="form-control" placeholder="Correo electrónico" id="correo" name="email">
+                </div>
+                <div class="mb-3">
+                    <input type="password" class="form-control" placeholder="Contraseña" id="contrasenna" name="password">
+                </div>
+                <button class="btn btn-outline-light" id="button-newsletter" type="submit"> {{ titleButtonSubmit }} </button>
+            </form>
+        </div>
+    </div>
+</template>
