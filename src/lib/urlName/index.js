19eb4d8a986227762688350d6ae25de1d3dc1268
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-07-24 15:00:42 -0400

    se almacenan los nombres de las rutas en el archivo urlName

---
+++ src/lib/urlName/index.js
@@ -0,0 +1,8 @@
+export const
+    urlInformationName = 'informacion',
+    urlSignInName = 'login',
+    urlHomeName = 'home',
+    urlSignUpName = 'home',
+    urlContactName = 'contacto',
+    urlCharacteristicsName = 'home',
+    urlLogoutName = 'home'
