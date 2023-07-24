1ea7bd067b4b9c4cb7296e7899d2d022736b4934
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-07-24 15:01:27 -0400

    vista para hacer login en la aplicación

---
+++ src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import SignIn from '../components/forms/SignIn.vue';
+    import { urlSignUpName } from '../lib/urlName'
+
+    const toSignUpUrl = { name: urlSignUpName, hash: '#sign-up' }
+    const titleSignUpButton = "Registrarse ahora."
+</script>
+
+<template>
+    <SignIn :toSignUpUrl="toSignUpUrl" :titleSignUpButton="titleSignUpButton" />
+</template>
