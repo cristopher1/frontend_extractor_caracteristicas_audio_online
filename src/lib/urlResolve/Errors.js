126ea8bde5adabe9860b5d6baf97220a0c43c560
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-07-27 23:41:29 -0400

    se modificaron los errores de urlResolve

--- src/lib/urlResolve/Errors.js
+++ src/lib/urlResolve/Errors.js
@@ -1,10 +1,3 @@
-export class NotExternalConstructorError extends Error {
-    constructor() {
-        super(`This constructor cannot called externally. Use getInstace method to obtain a instance`)
-        this.name = 'NotExternalConstructorError'
-    }
-}
-
 export class PathNotFoundError extends Error {
     constructor(message) {
         super(message)
