0ad0d2cf816f040b85007542b36eddc1eb849102
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-08-05 19:03:38 -0400

    uso de bootstrap y bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import './assets/css/views/main.css'
 
+import { BootstrapVue } from 'bootstrap-vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 const app = createApp(App)
 
 app.use(router)
+app.use(BootstrapVue)
 
 app.mount('#app')
