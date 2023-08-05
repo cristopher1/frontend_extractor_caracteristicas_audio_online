6f9ce56d8b0dd6ab40877089a7c83386190c46b9
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-08-05 19:04:28 -0400

    inyección de dependencias en httpRequest

--- src/lib/httpRequest/index.js
+++ src/lib/httpRequest/index.js
@@ -1,12 +1,26 @@
-import { IncorrectTypeError } from '../genericErrors'
-import { NotThereIsUrlError } from './Errors'
+import axios from 'axios'
 import { UrlResolveSingleton } from '../urlResolve'
 import { isTypeof } from '../genericFunctions'
+import { IncorrectTypeError } from '../genericErrors'
+import { NotThereIsUrlError } from './Errors'
 
 const urlResolve = UrlResolveSingleton.getInstance()
 
-const initCreateHttpRequest = (urlResolve, isType) => {
-    return (httpRequest, namePath = null, url = null) => {
+const refreshToken = async (url, httpClient, nameRefreshTokenItem, nameAccessTokenItem) => {
+    try {
+        const refreshTokenData = { refresh: localStorage.getItem(nameRefreshTokenItem) }
+        const response = await httpClient.post(url, refreshTokenData)
+        if (response.status === 200) {
+            const accessToken = response.data.access
+            localStorage.setItem(nameAccessTokenItem, accessToken)
+        } 
+    } catch (error) {
+        throw error
+    }
+}
+
+const initCreateHttpRequest = (urlResolve, isType, httpClient, refreshToken) => {
+    return function createHttpRequest(httpRequest, namePath = null, url = null) {
         if (!namePath && !url) {
             throw new NotThereIsUrlError(`Not there is url. You should use namePath or url parameters`)
         }
@@ -19,9 +33,20 @@ const initCreateHttpRequest = (urlResolve, isType) => {
             throw new IncorrectTypeError('url and namePath parameters, must be a string')
         }
         return async () => {
-            return await httpRequest(urlHttpRequest)
+            try {
+                try {
+                    await httpRequest(urlHttpRequest, httpClient)
+                }
+                catch (err) {
+                    refreshToken()
+                    await httpRequest(urlHttpRequest, httpClient)
+                }
+            }
+            catch (err) {
+                console.error(err.response)
+            }
         }
     }
 }
 
-export const createHttpRequest = initCreateHttpRequest(urlResolve, isTypeof)
+export const createHttpRequest = initCreateHttpRequest(urlResolve, isTypeof, axios, refreshToken)
